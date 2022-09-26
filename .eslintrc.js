fcfb7fa175a016fdb5afb8344a1c61d57cf734c5
Annie Chi <elmo1419141@gmail.com>
2022-09-26 15:48:23 +0800

    feat: day 01

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,47 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
-  parser: '@typescript-eslint/parser',
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module"
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ["react", "@typescript-eslint"],
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"]
+      }
+    }
+  },
   rules: {
-    '@typescript-eslint/no-unused-vars': 'warn',
-    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
-  },
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+    "no-unused-vars": "warn",
+    "no-use-before-define": ["warn", { variables: false }],
+    "react/function-component-definition": [
+      2,
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function"
+      }
+    ],
+    "react/jsx-filename-extension": [0, { extensions: [".js", ".jsx"] }],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never"
+      }
+    ]
+  }
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
-import styled from 'styled-components';
-import './App.css';
-import { GlobalStyle, ResetStyle } from './styles/globalStyle';
+import React from "react";
+import styled from "styled-components";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { GlobalStyle, ResetStyle } from "./styles/globalStyle";
 
-function App() {
-  return (
-    <Container>
-      <ResetStyle />
-      <GlobalStyle />
-    </Container>
-  );
-}
+import Day01 from "./pages/Day01";
+
+const App = () => (
+  <Container>
+    <ResetStyle />
+    <GlobalStyle />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/day-01" element={<Day01 />} />
+      </Routes>
+    </BrowserRouter>
+  </Container>
+);
 
 export default App;
 
 const Container = styled.div``;
-
